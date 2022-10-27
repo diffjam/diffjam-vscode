eec338a103d9182406d0379f832264ebda32566e
Will Weiss <will.weiss1230@classdojo.com>
2022-10-27 18:02:07 -0500

    updates for diffjam 3.1.0

--- src/diffjamDiagnostics.ts
+++ src/diffjamDiagnostics.ts
@@ -1,13 +1,17 @@
 import * as vscode from "vscode";
 import { DIFFJAM_RULE_BREACH } from "./extension";
 import { findBreachesInText } from "diffjam/lib/src/findBreachesInText";
-import { getConfig, refresh } from "diffjam/lib/src/configFile";
+import { Config } from "diffjam/lib/src/Config";
+import { GitIgnore } from "diffjam/lib/src/GitIgnore";
 import { LoggingService } from "./loggingService";
+import { join } from "path";
 
 const projectFolder = vscode.workspace.workspaceFolders?.map(
   (folder) => folder.uri.path
 )[0];
-export const configFile = `${projectFolder}/diffjam.yaml`;
+
+export const configFile = join(projectFolder, "diffjam.yaml");
+export const gitignoreFile = join(projectFolder, ".gitignore");
 
 // interface FileBreach {
 //   lineNumber: number; // the lineNumber, 1-indexed
@@ -16,63 +20,69 @@ export const configFile = `${projectFolder}/diffjam.yaml`;
 //   message: string; // the policy description.
 // }
 
-export async function refreshDiffJamConfig(logger?: LoggingService) {
-  await refresh(configFile);
+let config: Config;
+let gitignore: GitIgnore
+
+export async function refreshDiffJamConfig() {
+  config = await Config.read(configFile);
 }
 
-export function runDiffJamOnDocument(
+export async function refreshGitIgnore() {
+  gitignore = new GitIgnore(gitignoreFile);
+  await gitignore.ready;
+}
+
+export async function runDiffJamOnDocument(
   document: vscode.TextDocument,
   diagnosticCollection: vscode.DiagnosticCollection,
   logger?: LoggingService
 ) {
+  if (!config) await refreshDiffJamConfig();
   // run diffjam to find actual diagnostics
-  getConfig(configFile).then((config) => {
-    // quick no-op if policy is empty
-    if (config.getPolicyNames().length == 0) {
-      return;
-    }
 
-    const diagnostics: vscode.Diagnostic[] = [];
-    const fileName = document.fileName.replace(
-      projectFolder ? `${projectFolder}/` : "",
-      ""
-    );
-    const fileContents = document.getText();
+  // quick no-op if policy is empty
+  if (config.getPolicyNames().length == 0) {
+    return;
+  }
 
-    // we should be checking the filename matched the glob
-    findBreachesInText(fileName, fileContents, config)
-      .then((breaches) => {
-        const errors = breaches.map((b) => {
-          const startColumn = b.wholeLine.indexOf(b.found);
-          const endColumn = startColumn + b.found.length;
-          return {
-            line: Math.max(b.lineNumber, 2),
-            startColumn: Math.max(startColumn, 0),
-            endColumn,
-            msg: b.message,
-            severity: vscode.DiagnosticSeverity.Warning,
-          };
-        });
+  const diagnostics: vscode.Diagnostic[] = [];
+  const fileName = document.fileName.replace(
+    projectFolder ? `${projectFolder}/` : "",
+    ""
+  );
+  const fileContents = document.getText();
 
-        errors.forEach((error) => {
-          const range = new vscode.Range(
-            error.line - 1,
-            error.startColumn,
-            error.line - 1,
-            error.endColumn
-          );
-          const diagnostic = new vscode.Diagnostic(
-            range,
-            error.msg,
-            error.severity
-          );
-          diagnostic.source = "diffjam.yaml";
-          diagnostic.code = DIFFJAM_RULE_BREACH;
-          diagnostics.push(diagnostic);
-        });
+  // we should be checking the filename matched the glob
+  const breaches = findBreachesInText(fileName, fileContents, config, gitignore)
 
-        diagnosticCollection.set(document.uri, diagnostics);
-      })
-      .catch((err) => logger?.logError(err));
+  const errors = breaches.map((b) => {
+    const startColumn = b.wholeLine.indexOf(b.found);
+    const endColumn = startColumn + b.found.length;
+    return {
+      line: Math.max(b.lineNumber, 2),
+      startColumn: Math.max(startColumn, 0),
+      endColumn,
+      msg: b.message,
+      severity: vscode.DiagnosticSeverity.Warning,
+    };
   });
+
+  errors.forEach((error) => {
+    const range = new vscode.Range(
+      error.line - 1,
+      error.startColumn,
+      error.line - 1,
+      error.endColumn
+    );
+    const diagnostic = new vscode.Diagnostic(
+      range,
+      error.msg,
+      error.severity
+    );
+    diagnostic.source = "diffjam.yaml";
+    diagnostic.code = DIFFJAM_RULE_BREACH;
+    diagnostics.push(diagnostic);
+  });
+
+  diagnosticCollection.set(document.uri, diagnostics);
 }
