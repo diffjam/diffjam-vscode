7cb8c4c772c735f8f8c751ba869a9896edff4d41
Will Weiss <will.weiss1230@classdojo.com>
2022-11-07 11:17:12 -0600

    handle zero indexing

--- src/diffjamDiagnostics.ts
+++ src/diffjamDiagnostics.ts
@@ -8,17 +8,10 @@ import { join } from "path";
 
 const projectFolder = vscode.workspace.workspaceFolders?.map(
   (folder) => folder.uri.path
-)[0];
+)[0]!;
 
 export const configFile = join(projectFolder, "diffjam.yaml");
 export const gitignoreFile = join(projectFolder, ".gitignore");
-
-// interface FileBreach {
-//   lineNumber: number; // the lineNumber, 1-indexed
-//   found: string; // the match.  we can do squigglies based on its beginning/end
-//   wholeLine: string; // the entire line that the match is in.  used for subsequent needles.
-//   message: string; // the policy description.
-// }
 
 let config: Config;
 let gitignore: GitIgnore
@@ -45,7 +38,7 @@ export async function runDiffJamOnDocument(
     return;
   }
 
-  const diagnostics: vscode.Diagnostic[] = [];
+  [];
   const fileName = document.fileName.replace(
     projectFolder ? `${projectFolder}/` : "",
     ""
@@ -55,33 +48,21 @@ export async function runDiffJamOnDocument(
   // we should be checking the filename matched the glob
   const breaches = findBreachesInText(fileName, fileContents, config, gitignore)
 
-  const errors = breaches.map((b) => {
-    const startColumn = b.wholeLine.indexOf(b.found);
-    const endColumn = startColumn + b.found.length;
-    return {
-      line: Math.max(b.lineNumber, 2),
-      startColumn: Math.max(startColumn, 0),
-      endColumn,
-      msg: b.message,
-      severity: vscode.DiagnosticSeverity.Warning,
-    };
-  });
-
-  errors.forEach((error) => {
+  const diagnostics: vscode.Diagnostic[] = breaches.map((breach) => {
     const range = new vscode.Range(
-      error.line - 1,
-      error.startColumn,
-      error.line - 1,
-      error.endColumn
+      breach.startLineNumber,
+      breach.startColumn,
+      breach.endLineNumber,
+      breach.endColumn
     );
     const diagnostic = new vscode.Diagnostic(
       range,
-      error.msg,
-      error.severity
+      breach.message,
+      breach.severity
     );
     diagnostic.source = "diffjam.yaml";
     diagnostic.code = DIFFJAM_RULE_BREACH;
-    diagnostics.push(diagnostic);
+    return diagnostic
   });
 
   diagnosticCollection.set(document.uri, diagnostics);
